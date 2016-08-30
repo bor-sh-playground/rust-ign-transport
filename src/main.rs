3979cff1f1291db84fa0fb643216c9815377e84d
b-sh <b-sh@gmx.net>
2016-08-30 19:37:19 +0200

    extend with position setting and model name

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
+extern crate libc;
+use libc::{c_int, c_char};
+use std::ffi::CString;
+
+
 #[link(name = "gz_wrapper")]
 extern {
     pub fn run();
     pub fn pub_pos();
+    pub fn set_pos(s: *const c_char, x: c_int, y: c_int, z: c_int);
 }
 
 fn main() {
+    let model_name = CString::new("box").unwrap();
     unsafe {
-        pub_pos();
-//        run();
+        set_pos(model_name.as_ptr(), 4,0,0);
     }
 }
